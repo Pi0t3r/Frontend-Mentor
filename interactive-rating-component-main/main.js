3fc275975dc123d0f2f0550e474f1d1637ccb0f3
Pi0t3r <kapuscinskipiotr5@gmail.com>
2022-08-08 17:37:42 +0200

    rating script is done, not perfect but working

--- interactive-rating-component-main/main.js
+++ interactive-rating-component-main/main.js
@@ -16,6 +16,50 @@ const hideAndShow = () => {
 	thanks.classList.remove('none')
 }
 
+one.addEventListener('click', () => {
+	one.classList.toggle('active')
+})
+two.addEventListener('click', () => {
+	two.classList.toggle('active')
+})
+three.addEventListener('click', () => {
+	three.classList.toggle('active')
+})
+four.addEventListener('click', () => {
+	four.classList.toggle('active')
+})
+five.addEventListener('click', () => {
+	five.classList.toggle('active')
+})
 
-
+const addGrade1 = () => {
+    if(one.classList.contains('active')){
+        span.textContent = "1"
+    }
+}
+const addGrade2 = () => {
+    if(two.classList.contains('active')){
+        span.textContent = "2"
+    }
+}
+const addGrade3 = () => {
+    if(three.classList.contains('active')){
+        span.textContent = "3"
+    }
+}
+const addGrade4 = () => {
+    if(four.classList.contains('active')){
+        span.textContent = "4"
+    }
+}
+const addGrade5 = () => {
+    if(five.classList.contains('active')){
+        span.textContent = "5"
+    }
+}
+submit.addEventListener('click',addGrade1)
+submit.addEventListener('click',addGrade2)
+submit.addEventListener('click',addGrade3)
+submit.addEventListener('click',addGrade4)
+submit.addEventListener('click',addGrade5)
 submit.addEventListener('click', hideAndShow)
