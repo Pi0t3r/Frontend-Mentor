2cd99a54dc0588712c82e95dbdabac3ad2877fb2
Pi0t3r <kapuscinskipiotr5@gmail.com>
2022-08-15 18:49:12 +0200

    correct rwd to desktop on: 1440px x 800 px

--- interactive-card-details-form-main/main.js
+++ interactive-card-details-form-main/main.js
@@ -7,46 +7,56 @@ const btn = document.querySelector('.button')
 const pNumber = document.querySelector('.wrongNumber')
 const wrongDate = document.querySelector('.wrongDate')
 const inputNumber = document.querySelector('.inputNumber')
-
+const form = document.querySelector('.form')
+const complete = document.querySelector('.complete')
+let number2 = "432 32 42dcs"
 
 const checkDate = () => {
-	if (dateY.value.length === 0 || dateY.value.length >= 3 && dateM.value.length === 0 || dateM.value.length >= 3 ) {
+	if (dateY.value.length !== 2 && dateM.value.length !== 2) {
 		wrongDate.style.display = 'block'
 		dateY.style.border = '1px solid red'
-        dateM.style.border = '1px solid red'
+		dateM.style.border = '1px solid red'
 	} else if (dateY.value.length == 2 && dateY.value.length == 2) {
 		wrongDate.style.display = 'none'
-        dateY.style.border = '1px solid #dedddf'
-        dateM.style.border = '1px solid #dedddf'
+		dateY.style.border = '1px solid #dedddf'
+		dateM.style.border = '1px solid #dedddf'
 	}
 }
 
-const checkNumber = () => { 
-    if(inputNumber.value.length !== 12){
-        pNumber.style.display = 'block'
-        inputNumber.style.border = '1px solid red'
-    }else if(inputNumber.value.length === 12){
-        pNumber.style.display = 'none'
-        inputNumber.style.border = '1px solid #dedddf'
-    }
- }
+const checkNumber = () => {
+	if (inputNumber.value.length !== 12) {
+		pNumber.style.display = 'block'
+		inputNumber.style.border = '1px solid red'
+	} else if (inputNumber.value.length === 12) {
+		pNumber.style.display = 'none'
+		inputNumber.style.border = '1px solid #dedddf'
+	}
+}
 
-const checkCvc = () => { 
-    if(cvc.value.length !== 3){
-        wrongCvc.style.display = 'block'
-        cvc.style.border = '1px solid red'
-    }else if(cvc.value.length === 3){
-        wrongCvc.style.display = 'none'
-        cvc.style.border = '1px solid #dedddf'
+const checkCvc = () => {
+	if (cvc.value.length !== 3) {
+		wrongCvc.style.display = 'block'
+		cvc.style.border = '1px solid red'
+	} else if (cvc.value.length === 3) {
+		wrongCvc.style.display = 'none'
+		cvc.style.border = '1px solid #dedddf'
+	}
+}
+
+const allGood = () => {
+    if(dateY.value.length == 2 && dateY.value.length == 2 && inputNumber.value.length === 12 && cvc.value.length === 3){
+        form.classList.remove('block')
+        form.classList.add('none')
+        complete.classList.remove('none')
+        complete.classList.add('block')
     }
- }
+}
 
 const checkAll = () => {
 	checkDate()
-    checkNumber()
-    checkCvc()
+	checkNumber()
+	checkCvc()
+    allGood()
 }
-
-
 
 btn.addEventListener('click', checkAll)
