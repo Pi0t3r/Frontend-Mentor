f4e8def5ebdab60cdb35397debd5352f6a442bb5
Pi0t3r <kapuscinskipiotr5@gmail.com>
2022-08-15 17:40:32 +0200

    added function to: cvc, card number without problem with letters and exp date

--- interactive-card-details-form-main/main.js
+++ interactive-card-details-form-main/main.js
@@ -2,29 +2,51 @@ const number = document.querySelector('.inputNumber')
 const dateM = document.querySelector('.dateM')
 const dateY = document.querySelector('.dateY')
 const cvc = document.querySelector('.inputCvc')
+const wrongCvc = document.querySelector('.wrongCvc')
 const btn = document.querySelector('.button')
 const pNumber = document.querySelector('.wrongNumber')
 const wrongDate = document.querySelector('.wrongDate')
+const inputNumber = document.querySelector('.inputNumber')
 
-const checknumber = () => {
-	if (number.textContent.contains('a-zA-Z')) {
-		pNumber.classList.remove('none')
-		pNumber.classList.add('block')
+
+const checkDate = () => {
+	if (dateY.value.length === 0 || dateY.value.length >= 3 && dateM.value.length === 0 || dateM.value.length >= 3 ) {
+		wrongDate.style.display = 'block'
+		dateY.style.border = '1px solid red'
+        dateM.style.border = '1px solid red'
+	} else if (dateY.value.length == 2 && dateY.value.length == 2) {
+		wrongDate.style.display = 'none'
+        dateY.style.border = '1px solid #dedddf'
+        dateM.style.border = '1px solid #dedddf'
 	}
 }
 
-const checkDateM = () => {
-    if(dateM.value.length == 0 || dateM.value.length >=3){
-        wrongDate.style.display ='block'
-        dateM.style.border = '1px solid red'
+const checkNumber = () => { 
+    if(inputNumber.value.length !== 12){
+        pNumber.style.display = 'block'
+        inputNumber.style.border = '1px solid red'
+    }else if(inputNumber.value.length === 12){
+        pNumber.style.display = 'none'
+        inputNumber.style.border = '1px solid #dedddf'
     }
-}
-const checkDateY = () => {
-    if(dateY.value.length == 0 || dateY.value.length >=3){
-        wrongDate.style.display ='block'
-        dateY.style.border = '1px solid red'
+ }
+
+const checkCvc = () => { 
+    if(cvc.value.length !== 3){
+        wrongCvc.style.display = 'block'
+        cvc.style.border = '1px solid red'
+    }else if(cvc.value.length === 3){
+        wrongCvc.style.display = 'none'
+        cvc.style.border = '1px solid #dedddf'
     }
+ }
+
+const checkAll = () => {
+	checkDate()
+    checkNumber()
+    checkCvc()
 }
-btn.addEventListener('click',checkDateM)
-btn.addEventListener('click',checkDateY)
-btn.addEventListener('click', checknumber)
+
+
+
+btn.addEventListener('click', checkAll)
