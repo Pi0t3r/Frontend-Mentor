d5963b1ea5da7aeb4999b3460d71d71486bf3509
Pi0t3r <kapuscinskipiotr5@gmail.com>
2022-08-28 15:03:56 +0200

    added function add and substract in input

--- ecommerce-product-page-main/main.js
+++ ecommerce-product-page-main/main.js
@@ -1,6 +1,9 @@
 const openMenu = document.querySelector('.open')
 const menu = document.querySelector('.side-menu')
 const close = document.querySelector('.btn-close')
+const minus = document.querySelector('.minus')
+const plus = document.querySelector('.plus')
+const howMany = document.querySelector('.howMany')
 
 const openSideMenu = () => {
 	menu.classList.remove('hide')
@@ -8,5 +11,17 @@ const openSideMenu = () => {
 const closeSideMenu = () => {
 	menu.classList.add('hide')
 }
-close.addEventListener('click',closeSideMenu)
+
+const add = () => {
+    howMany.value ++
+}
+const substract = () => { 
+    howMany.value --
+    if(howMany.value < 0 ){
+        howMany.value = 0
+    }
+ }
+ minus.addEventListener('click',substract)
+plus.addEventListener('click',add)
+close.addEventListener('click', closeSideMenu)
 openMenu.addEventListener('click', openSideMenu)
